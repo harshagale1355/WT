786b2a180ea3acf95d87bf539106e72200fae225
Harsh Agale <harshagale1355@gmail.com>
2025-11-29 19:15:58 +0530

    second commit

--- factorial.php
+++ factorial.php
@@ -1,6 +1,16 @@
 <?php
-$number = 
+$number = $_POST['num'];
 
+function factorial_number($number){
+    $fact=1;
+    for($i=1;$i<=$number;$i++){
+        $fact=$fact*$i;
+    }
+    return $fact;
+}
+
+echo "result";
+echo "The factorial of number ". $number . " is ".factorial_number($number);
 
 
 
